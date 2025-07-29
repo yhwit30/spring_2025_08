516e4c8e6103c8dd4fae975b98e2ea836e0eaf7e
yhwit30 <yhwit30@gmail.com>
2025-07-29 20:55:40 +0900

    프로젝트 실행환경 세팅

---
+++ src/main/java/com/example/demo/controller/UsrHomeController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class UsrHomeController {
+	
+	@RequestMapping("/usr/home/main")
+	@ResponseBody
+	public String showMain() {
+		return "안녕하세요";
+	}
+
+}
