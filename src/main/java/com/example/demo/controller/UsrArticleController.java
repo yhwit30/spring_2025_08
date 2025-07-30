80097aabb313e1857dd19078476be3ee4520626d
yhwit30 <yhwit30@gmail.com>
2025-07-30 20:23:30 +0900

    게시글 테스트데이터 생성

--- src/main/java/com/example/demo/controller/UsrArticleController.java
+++ src/main/java/com/example/demo/controller/UsrArticleController.java
@@ -18,16 +18,33 @@ public class UsrArticleController {
 	public UsrArticleController() {
 		this.lastArticleId = 0;
 		this.articles = new ArrayList<>();
+		
+		makeTestData();
+		
+	}
+	private void makeTestData() {
+		
+		for(int i = 1; i <= 10; i++) {
+			String title = "제목 " + i;
+			String body = "내용 " + i;
+			writeArticle(title, body);
+			
+		}
+	}
+	private Article writeArticle(String title, String body) {
+		int id = ++this.lastArticleId;
+		
+		Article article = new Article(id, title, body);
+		this.articles.add(article);
+
+		return article;
 	}
 
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
-
-		int id = this.lastArticleId + 1;
-
-		Article article = new Article(id, title, body);
-		this.articles.add(article);
+		
+		Article article = writeArticle(title, body);
 
 		return article;
 	}
