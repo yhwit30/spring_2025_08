82ba5e3130590eed4a870ca72b1e6a09e2b26e94
yhwit30 <yhwit30@gmail.com>
2025-07-31 21:27:15 +0900

    마이바티스 xml방식으로 변경

--- src/main/java/com/example/demo/repository/ArticleRepository.java
+++ src/main/java/com/example/demo/repository/ArticleRepository.java
@@ -2,30 +2,26 @@ package com.example.demo.repository;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
 
 import com.example.demo.vo.Article;
 
 @Mapper
 public interface ArticleRepository {
 
-	@Insert("INSERT INTO `article` SET `regDate` = NOW(), `updateDate` = NOW(), `title` = #{title}, `body` = #{body};")
+//	@Insert("INSERT INTO `article` SET `regDate` = NOW(), `updateDate` = NOW(), `title` = #{title}, `body` = #{body};")
 	public int writeArticle(String title, String body);
 
-	@Delete("DELETE FROM `article` WHERE `id` = #{id}")
+//	@Delete("DELETE FROM `article` WHERE `id` = #{id}")
 	public void deleteArticle(int id);
 
-	@Update("UPDATE `article` SET `updateDate` = NOW(), `title` = #{title}, `body` = #{body} WHERE `id` = #{id}")
+//	@Update("UPDATE `article` SET `updateDate` = NOW(), `title` = #{title}, `body` = #{body} WHERE `id` = #{id}")
 	public Object modifyArticle(int id, String title, String body);
 
-	@Select("SELECT * FROM `article` WHERE `id` = #{id}")
+//	@Select("SELECT * FROM `article` WHERE `id` = #{id}")
 	public Article getArticleById(int id);
 
-	@Select("SELECT * FROM `article`")
+//	@Select("SELECT * FROM `article`")
 	public List<Article> getArticles();
 
 }
--- src/main/java/com/example/demo/vo/Article.java
+++ src/main/java/com/example/demo/vo/Article.java
@@ -3,14 +3,12 @@ package com.example.demo.vo;
 import java.time.LocalDateTime;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
 public class Article {
 
 	int id;
