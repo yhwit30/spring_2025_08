e04080c31cf3c6f992636f1a3e22959a272960c7
yhwit30 <yhwit30@gmail.com>
2025-08-14 19:23:45 +0900

    DB 스키마 수정

--- DB.sql
+++ DB.sql
@@ -28,6 +28,32 @@ CREATE TABLE `member` (
     `delDate` DATETIME COMMENT '탈퇴 날짜'
 );
 
+# 게시판 테이블 생성
+CREATE TABLE `board`(
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `regDate` DATETIME NOT NULL,
+    `updateDate` DATETIME NOT NULL,
+    `code` CHAR(50) NOT NULL UNIQUE COMMENT 'notice(공지사항) free(자유) QnA(질의응답)',
+    `name` CHAR(20) NOT NULL UNIQUE COMMENT '게시판 이름',
+    `delStatus` TINYINT(1) UNSIGNED DEFAULT 0 COMMENT '탈퇴 여부(0=탈퇴 전, 1=탈퇴 후)',
+    `delDate` DATETIME COMMENT '탈퇴 날짜'
+);
+
+INSERT INTO `board`
+SET `regDate` = NOW(),
+    `updateDate` = NOW(),
+    `code` = 'notice',
+    `name` = '게시판';
+INSERT INTO `board`
+SET `regDate` = NOW(),
+    `updateDate` = NOW(),
+    `code` = 'free',
+    `name` = '자유';
+INSERT INTO `board`
+SET `regDate` = NOW(),
+    `updateDate` = NOW(),
+    `code` = 'QnA',
+    `name` = '질의응답';
 
 INSERT INTO `article`
 SET `regDate` = NOW(),
@@ -44,7 +70,16 @@ SET `regDate` = NOW(),
     `updateDate` = NOW(),
     `title` = '제목3',
     `body` = '내용3';    
-
+INSERT INTO `article`
+SET `regDate` = NOW(),
+    `updateDate` = NOW(),
+    `title` = '제목4',
+    `body` = '내용4'; 
+INSERT INTO `article`
+SET `regDate` = NOW(),
+    `updateDate` = NOW(),
+    `title` = '제목5',
+    `body` = '내용5'; 
 
 INSERT INTO `member`
 SET `regDate` = NOW(),
@@ -78,25 +113,48 @@ SET `regDate` = NOW(),
     `email` = '회원2@gmail.com'; 
 
 ALTER TABLE `article` ADD COLUMN `memberId` INT UNSIGNED NOT NULL AFTER `updateDate`;
-
 UPDATE `article` 
 SET `memberId` = 2
 WHERE `id` IN (1,2);
-
 UPDATE `article` 
 SET `memberId` = 1
 WHERE `id` IN (3);
+UPDATE `article` 
+SET `memberId` = 3
+WHERE `id` IN (4,5);
+
+
+ALTER TABLE `article` ADD COLUMN `boardId` INT UNSIGNED NOT NULL AFTER `memberId`;
+UPDATE `article` 
+SET `boardId` = 1
+WHERE `id` IN (1,2);
+UPDATE `article` 
+SET `boardId` = 2
+WHERE `id` IN (3,4);
+UPDATE `article` 
+SET `boardId` = 3
+WHERE `id` IN (5);
+
 
 SELECT *
 FROM `article`;
 SELECT *
 FROM `member`;
 
+
+SELECT a.*, m.nickname
+		FROM `article` a
+		INNER JOIN `member` m
+		ON a.memberId = m.id
+		WHERE a.`id` = 1
+
 ###############################################
 # 게시글 데이터 대량 생성
 INSERT INTO `article`
 SET `regDate` = NOW(),
     `updateDate` = NOW(),
+    `memberId` = CEILING(RAND() * 3),
+    `boardId` = CEILING(RAND() * 3),
     `title` = CONCAT('제목', SUBSTRING(RAND() * 1000 FROM 1 FOR 2)),
     `body` = CONCAT('내용', SUBSTRING(RAND() * 1000 FROM 1 FOR 2));
 
