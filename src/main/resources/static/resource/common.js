37daa7cc5cb7e94952caecce5f757b07f4b1ee73
yhwit30 <yhwit30@gmail.com>
2025-08-14 18:45:52 +0900

    데이제ui 적용 수정, 테마토글 js 추가

--- src/main/resources/static/resource/common.js
+++ src/main/resources/static/resource/common.js
@@ -1,3 +1,16 @@
-/**
- * 
- */
+// 테마 토글
+$(function () {
+  const $toggle = $("#themeToggle");
+  const initial = localStorage.getItem("theme") || "light";
+  setTheme(initial);
+  $toggle.prop("checked", initial !== "light");
+
+  $toggle.on("change", function () {
+    setTheme(this.checked ? "dark" : "light");
+  });
+
+  function setTheme(theme) {
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  }
+});
